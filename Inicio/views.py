78e26a3bd6a0c06579f38438a517a4c1c6207814
MTcloud2023 <153304839+MTcloud2023@users.noreply.github.com>
2025-09-06 04:09:19 -0600

    Update views.py

--- Inicio/views.py
+++ Inicio/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render 
 from django.http import HttpResponse
 from datetime import date
-
+import requests
+from django.shortcuts import render
 def hola(request):
         return render(request, 'inicio.html')
     
